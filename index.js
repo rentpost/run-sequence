69937c270f5006d478e1c7792fca28c04d8c147b
Jacob Thomason <jacob@thomason.xxx>
2014-11-24 01:53:34 -0500

    Refactored the entire module

--- index.js
+++ index.js
@@ -4,10 +4,93 @@
 
 var colors = require('chalk');
 
-function verifyTaskSets(gulp, taskSets, skipArrays) {
+
+module.exports = RunSequence;
+
+
+/**
+ * RunSequence Constructor
+ * @param {object} gulp		Pass gulp to the constructor for dependency handling
+ * @constructor
+ */
+function RunSequence(gulp) {
+	if(!(this instanceof RunSequence))
+		return new RunSequence(gulp);
+
+	this._gulp = gulp || require('gulp');
+}
+
+
+/**
+ * Starts the actual execution of the sequence of tasks via Orchestrator
+ * @param {Array} taskSets
+ * @param {function} callback
+ */
+RunSequence.prototype.start = function(taskSets, callback) {
+	var self = this,
+		currentTaskSet;
+
+	taskSets = taskSets || [];
+	callback = typeof callback === 'function' ? callback : false;
+
+	var finish = function (err) {
+			self._gulp.removeListener('task_stop', onTaskEnd);
+			self._gulp.removeListener('task_err', onError);
+			if(callback) {
+				callback(err);
+			} else if (err) {
+				console.log(colors.red('Error running task sequence:'), err);
+			}
+		},
+
+		onError = function (err) {
+			finish(err);
+		},
+
+		onTaskEnd = function (event) {
+			var idx = currentTaskSet.indexOf(event.task);
+			if (idx > -1) {
+				currentTaskSet.splice(idx, 1);
+			}
+			if (currentTaskSet.length === 0) {
+				runNextSet();
+			}
+		},
+
+		runNextSet = function () {
+			if (taskSets.length) {
+				var command = taskSets.shift();
+				if (!Array.isArray(command)) {
+					command = [command];
+				}
+				currentTaskSet = command;
+				self._gulp.start.apply(self._gulp, command);
+			} else {
+				finish();
+			}
+		};
+
+	this.verifyTaskSets(taskSets);
+
+	self._gulp.on('task_stop', onTaskEnd);
+	self._gulp.on('task_err', onError);
+
+	runNextSet();
+};
+
+
+/**
+ * Verification of the task sets provided
+ * @param taskSets
+ * @param skipArrays
+ */
+RunSequence.prototype.verifyTaskSets = function(taskSets, skipArrays) {
+	var self = this;
+
 	if(taskSets.length === 0) {
 		throw new Error('No tasks were provided to run-sequence');
 	}
+
 	var foundTasks = {};
 	taskSets.forEach(function(t) {
 		var isTask = typeof t === "string",
@@ -15,8 +98,8 @@ function verifyTaskSets(gulp, taskSets, skipArrays) {
 		if(!isTask && !isArray) {
 			throw new Error("Task "+t+" is not a valid task string.");
 		}
-		if(isTask && !gulp.hasTask(t)) {
-			throw new Error("Task "+t+" is not configured as a task on gulp.  If this is a submodule, you may need to use require('run-sequence').use(gulp).");
+		if(isTask && !self._gulp.hasTask(t)) {
+			throw new Error("Task "+t+" is not configured as a task on gulp.");
 		}
 		if(skipArrays && isTask) {
 			if(foundTasks[t]) {
@@ -28,64 +111,7 @@ function verifyTaskSets(gulp, taskSets, skipArrays) {
 			if(t.length === 0) {
 				throw new Error("An empty array was provided as a task set");
 			}
-			verifyTaskSets(gulp, t, true, foundTasks);
+			self.verifyTaskSets(t, true, foundTasks);
 		}
 	});
-}
-
-function runSequence(gulp) {
-	// Slice and dice the input to prevent modification of parallel arrays.
-	var taskSets = Array.prototype.slice.call(arguments, 1).map(function(task) {
-			return Array.isArray(task) ? task.slice() : task;
-		}),
-		callBack = typeof taskSets[taskSets.length-1] === 'function' ? taskSets.pop() : false,
-		currentTaskSet,
-
-		finish = function(err) {
-			gulp.removeListener('task_stop', onTaskEnd);
-			gulp.removeListener('task_err', onError);
-			if(callBack) {
-				callBack(err);
-			} else if(err) {
-				console.log(colors.red('Error running task sequence:'), err);
-			}
-		},
-
-		onError = function(err) {
-			finish(err);
-		},
-		onTaskEnd = function(event) {
-			var idx = currentTaskSet.indexOf(event.task);
-			if(idx > -1) {
-				currentTaskSet.splice(idx,1);
-			}
-			if(currentTaskSet.length === 0) {
-				runNextSet();
-			}
-		},
-
-		runNextSet = function() {
-			if(taskSets.length) {
-				var command = taskSets.shift();
-				if(!Array.isArray(command)) {
-					command = [command];
-				}
-				currentTaskSet = command;
-				gulp.start.apply(gulp, command);
-			} else {
-				finish();
-			}
-		};
-
-	verifyTaskSets(gulp, taskSets);
-
-	gulp.on('task_stop', onTaskEnd);
-	gulp.on('task_err', onError);
-
-	runNextSet();
-}
-
-module.exports = runSequence.bind(null, require('gulp'));
-module.exports.use = function(gulp) {
-	return runSequence.bind(null, gulp);
 };
